29acd7ebf377e55648f222c69ea0347089c56238
ChrisChroma <chrischroma1030@gmail.com>
2020-05-22 10:53:25 -0500

    route and form work

---
+++ controllers/flights.js
@@ -0,0 +1,19 @@
+const flight = require('../models/flight');
+
+module.exports = {
+    index,
+    new: newFlightView,
+    create
+}
+
+function index(req, res) {
+    res.render('/flights/index');
+}
+
+function newFlightView(req, res) {
+    res.render('flights/new');
+}
+
+function create(req, res){
+    
+}
--- models/flight.js
+++ models/flight.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: {
-        type: String,
-        required: true,
-        enum: ["American", "Southwest", "United"]
+  airline: {
+    type: String,
+    required: true,
+    enum: ["American", "Southwest", "United"],
+  },
+  airport: {
+    type: String,
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    default: "DEN",
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999,
+  },
+  departs: {
+    type: Date,
+    default: function () {
+      const defaultDate = new Date();
+      defaultDate.setFullYear(defaultDate.getFullYear() + 1);
+      return defaultDate();
     },
-    airport: {
-        type: String,
-        enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
-        default: "DEN"
-    },
-    flightNo: {
-        type: Number,
-        required: true,
-        min: 10,
-        max: 9999
-    },
-    departs: {
-        type: Date,
-        default: function(){
-            const defaultDate = new Date();
-            defaultDate.setFullYear(defaultDate.getFullYear() + 1);
-            return defaultDate();
-        }
-    }
+  },
 });
 
-
-module.exports = mongoose.model('Flight', flightSchema);
+module.exports = mongoose.model("Flight", flightSchema);
